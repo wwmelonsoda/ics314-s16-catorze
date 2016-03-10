9de85c418bb9b7e2be338f2ddeffa2b07f65ddd1
Kristy ww <kristyaw@hawaii.edu>
2016-03-09 17:16:11 -1000

    Allow user input
    Changed entire code to allow basic user input for ONE event into the
    .ics file

--- iCal.java
+++ iCal.java
@@ -1,16 +1,220 @@
 
 import java.io.BufferedWriter;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 //EDIT TEST FOR ECLIPSE AND GITHUB SYNC!!
 
 public class iCal {
 	public static void main(String[] args) {
+		
+        Scanner in = new Scanner(System.in);	
+		System.out.println("Welcome! Please add a new event to the calendar!");
+
+//EVENT NAME INPUT
+		System.out.print("Event Name:");
+		String EventName = in.nextLine();
+		
+//DATE START INPUT		
+		String EventStartDate ="";
+		boolean dateFlag = true;
+		do{
+		try {
+			System.out.print("Start Date:");
+			EventStartDate = in.nextLine();
+			//Format of the date expected in EventStartDate string
+			SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+			//Converting the EventStartDate String to Date
+			Date date = format.parse(EventStartDate);
+			//Desired format: yyyyMMdd
+			format = new SimpleDateFormat("yyyyMMdd");
+			//Changing the format of date and storing it in String
+			EventStartDate = format.format(date);
+		       dateFlag = false; //ends loop if correct format used
+		} catch (ParseException e) {
+			System.out.println("! ! valid dates in MM/DD/YYYY format ! !" );
+		}
+		}while (dateFlag == true); 
+		
+//DATE END INPUT		
+				String EventEndDate ="";
+				dateFlag = true;
+				do{
+				try {
+					System.out.print("End Date:");
+					EventEndDate = in.nextLine();
+					//Format of the date expected in EventEndDate string
+					SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+					//Converting the EventEndDate String to Date
+					Date date = format.parse(EventEndDate);
+					//Desired format: yyyyMMdd
+					format = new SimpleDateFormat("yyyyMMdd");
+					//Changing the format of date and storing it in String
+					EventEndDate = format.format(date);
+				       dateFlag = false; //ends loop if correct format used
+				} catch (ParseException e) {
+					System.out.println("! ! valid dates in MM/DD/YYYY format ! !" );
+				}
+				}while (dateFlag == true); 		
+	
+		
+		
+//TIME START INPUT
+		String EventStartTime ="";
+		boolean timeFlag = true;
+		
+		do{
+		try {
+			System.out.print("Start Time:");
+			EventStartTime = in.nextLine();
+			//Format of the date expected in EventStartTime string
+			DateFormat tFormat = new SimpleDateFormat("hh:mm aa");
+			//Desired format: 24 hour format
+			DateFormat outputformat = new SimpleDateFormat("HHmmss");
+			Date asDate = null;
+			
+	         //Converting the input String to Date
+	    	 asDate= tFormat.parse(EventStartTime);
+	         //Changing the format of date and storing it in String
+	    	 EventStartTime = outputformat.format(asDate);
+	    	 timeFlag = false; //ends loop if correct format used
+	      }catch(ParseException pe){
+	    	  System.out.println("! ! valid time in hh:mm am/pm ! ! " );
+	       }	
+		   }while (timeFlag == true); 	
+		
+		
+//TIME END INPUT
+		String EventEndTime ="";
+		timeFlag = true;
+		
+		do{
+		try {
+			System.out.print("End Time:");
+			EventEndTime = in.nextLine();
+			//Format of the date expected in EventEndTime string
+			DateFormat tFormat = new SimpleDateFormat("hh:mm aa");
+			//Desired format: 24 hour format
+			DateFormat outputformat = new SimpleDateFormat("HHmmss");
+			Date asDate = null;
+			
+	         //Converting the EventEndTime String to Date
+	    	 asDate= tFormat.parse(EventEndTime);
+	         //Changing the format of date and storing it in String
+	    	 EventEndTime = outputformat.format(asDate);
+	    	 timeFlag = false; //ends loop if correct format used
+	      }catch(ParseException pe){
+	    	  System.out.println("! ! valid time in hh:mm am/pm ! ! " );
+	       }	
+		   }while (timeFlag == true); 
+		
+//PLACE INPUT		
+		System.out.print("Place:");
+		String EventPlace = in.nextLine();
+
+//GEO LOCATION INPUT
+		System.out.print("Add Geo Location? (y/n):");
+		String Geo = in.nextLine();
+		String EventGeo ="";
+		String EventGeoLat ="";
+		String EventGeoLon ="";
+		float GeoLat = 0;
+		float GeoLon = 0;
+		
+		if(Geo.equalsIgnoreCase("y"))
+		{	
+			boolean latFlag = true;
+			boolean lonFlag = true;
+			System.out.println("Geo coordinates...");
+			do{
+			try
+		    { 
+			System.out.print("Lattitude:");
+		    GeoLat = in.nextFloat();	
+		    in.nextLine();
+		       latFlag = false;
+		    }catch ( InputMismatchException ex )
+		    { 
+		      System.out.println("! ! You Must Enter a valid coordinate ! !" );
+		      in.next(); //clears wrong input
+		    }
+			}while (latFlag == true);
+			
+			do{
+			try
+		    { 
+			System.out.print("Longitute:");
+		    GeoLon = in.nextFloat();	
+		    in.nextLine();
+		       lonFlag = false;
+		    }catch ( InputMismatchException ex )
+		    { 
+		      System.out.println("! ! You Must Enter a valid coordinate ! !" );
+		      in.next(); //clears wrong input
+		    }
+			}while (lonFlag == true);
+		    
+			EventGeoLat = String.valueOf(GeoLat);
+			EventGeoLon = String.valueOf(GeoLon);
+		}
+		
+		System.out.print("Additional notes:");
+		String EventDesc = in.nextLine();
+		
+		System.out.print("Public/Private?:");
+		String EventClass = in.nextLine();
+			if(EventClass.equalsIgnoreCase("public"))
+			{
+				EventClass = "PUBLIC";
+			}
+			if(EventClass.equalsIgnoreCase("private"))
+			{
+				EventClass = "PRIVATE";
+			}
+			else
+			{
+				System.out.println("Invalid Entry, set as Default: PUBLIC");
+				EventClass = "PUBLIC";
+			}
+		
+		System.out.println("\nDone.");
+		    // }
+		
 		try {
 
-			String content = "BEGIN:VCALENDAR\n" + "METHOD:PUBLISH\n" + "VERSION:2.0\n" + "X-WR-CALNAME:ics314\n" + "PRODID:-//Apple Inc.//Mac OS X 10.11.2//EN\n" + "X-APPLE-CALENDAR-COLOR:#FF9500\n" + "X-WR-TIMEZONE:Pacific/Honolulu\n" + "CALSCALE:GREGORIAN\n" + "BEGIN:VTIMEZONE\n" + "TZID:Pacific/Honolulu\n" + "BEGIN:DAYLIGHT\n" + "TZOFFSETFROM:-1030\n" + "DTSTART:19330430T020000\n" + "TZNAME:HDT\n" + "TZOFFSETTO:-0930\n" + "RDATE:19330430T020000\n" + "RDATE:19420209T020000\n" + "END:DAYLIGHT\n" + "BEGIN:STANDARD\n" + "TZOFFSETFROM:-1030\n" + "DTSTART:19470608T020000\n" + "TZNAME:HST\n" + "TZOFFSETTO:-1000\n" + "RDATE:19470608T020000\n" + "END:STANDARD\n" + "END:VTIMEZONE\n" + "BEGIN:VEVENT\n" + "CREATED:20160223T231946Z\n" + "UID:E552196C-4BBD-4E4F-96B3-8D93D3FD1C26\n" + "DTEND;TZID=Pacific/Honolulu:20160225T160000\n" + "TRANSP:OPAQUE\n" + "X-APPLE-TRAVEL-ADVISORY-BEHAVIOR:AUTOMATIC\n" + "SUMMARY:Meet with TA Amy\n" + "DTSTART;TZID=Pacific/Honolulu:20160225T150000\n" + "DTSTAMP:20160223T232045Z\n" + "SEQUENCE:0\n" + "END:VEVENT\n" + "END:VCALENDAR\n";
+			
+			String startCal = "BEGIN:VCALENDAR\n" 
+			+ "VERSION:2.0\n"
+			+ "PRODID:-//ZContent.net//Zap Calendar 1.0//EN\n"
+            + "CALSCALE:GREGORIAN\n"
+			+ "METHOD:PUBLISH\n";
+            
+			String Event = "BEGIN:VEVENT"
+            + "\nSUMMARY:" + EventName 
+            //+ "\nUID:"
+            //+ "\nSEQUENCE:0"
+            + "\nSTATUS:CONFIRMED"
+            + "\nCLASS:" + EventClass
+            + "\nTRANSP:TRANSPARENT"
+            + "\nDTSTART:" + EventStartDate + "T" + EventStartTime
+            + "\nDTEND:" + EventEndDate + "T" + EventEndTime
+            + "\nCATEGORIES: uncategorized"
+            + "\nLOCATION:" + EventPlace
+            + "\nGEO:" + EventGeoLat + ";" + EventGeoLon
+            + "\nDESCRIPTION:" + EventDesc
+            + "\nEND:VEVENT\n";
+			
+ String endCal = "END:VCALENDAR\n";
 
 			File file = new File("ical_catorze.ics");
 
@@ -21,10 +225,10 @@ public class iCal {
 
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(content);
+			bw.write(startCal + Event + endCal);
 			bw.close();
 
-			System.out.println("Done");
+			System.out.println("Calendar file has been created!");
 
 		} catch (IOException e) {
 			e.printStackTrace();
